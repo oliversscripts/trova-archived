8528511e0d421252ff2376c5fa51a5e13ccf58bb
oliversscripts <olivers.private@gmail.com>
2018-09-26 18:17:20 -0400

    Main urls update

--- trova/urls.py
+++ trova/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='base.login.html',)),
     path('logout/', views.LogoutView.as_view(next_page='/login/')),
     path('admin/', admin.site.urls),
-    path('apps/', include('apps.urls'))
+    path('apps/', include('apps.urls')),
 ]
