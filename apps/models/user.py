331402a7366f3e89f3cfe4edbaf878c08d911da6
oliversscripts <olivers.private@gmail.com>
2018-09-26 19:53:37 -0400

    Added trova_admin flag to users table

--- apps/models/user.py
+++ apps/models/user.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     plex_id = models.CharField(max_length=254, blank=True)
+    trova_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s' % (self.username)
