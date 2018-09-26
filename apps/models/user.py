7276f40c1e7137a82b1d0afa50c33504b63841aa
oliversscripts <olivers.private@gmail.com>
2018-09-26 13:12:29 -0400

    Added plexId to user

--- apps/models/user.py
+++ apps/models/user.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    plex_id = models.TextField(max_length=500, blank=True)
+    plex_id = models.CharField(max_length=254, blank=True)
