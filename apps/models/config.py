fc6a850b94fab15db1709f3d70626af24a6f441e
oliversscripts <olivers.private@gmail.com>
2018-09-27 06:48:45 -0400

    Removed dbsettigns

--- apps/models/config.py
+++ apps/models/config.py
@@ -1,6 +1,1 @@
 from django.db import models
-import dbsettings
-
-class ConfigTv(dbsettings.Group):
-    enabled = dbsettings.BooleanValue('is tv enabled')
-    trakt_api_key = dbsettings.StringValue('trakt api key')
