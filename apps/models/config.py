c51f2563d9585c0b344f6ebb9133bad413b8e46f
oliversscripts <olivers.private@gmail.com>
2018-09-27 12:53:42 -0400

    Additional fields to TV config model

--- apps/models/config.py
+++ apps/models/config.py
@@ -2,4 +2,11 @@ from django.db import models
 
 class TvConfig(models.Model):
     enabled = models.BooleanField('TV Enabled', default=True)
-    trakt_api_key = models.CharField('Trakt API Key', max_length=254, blank=True)
+    
+    api_key_trakt = models.CharField('Trakt API Key', max_length=254, blank=True)
+    api_key_tvdb = models.CharField('TVDB API Key', max_length=254, blank=True)
+
+    suggestions_enabled = models.BooleanField('Suggestions Enabled', default=True)
+    suggestions_trending_enabled = models.BooleanField('Trending Shows Enabled', default=True)
+    suggestions_popular_enabled = models.BooleanField('Popular Shows Enabled', default=True)
+    suggestions_anticipated_enabled = models.BooleanField('Anticipated Shows Enabled', default=True)
