e956692ab726e66f9b0a3e9d04082ed2dfcd04c4
oliversscripts <olivers.private@gmail.com>
2018-09-27 17:13:10 -0400

    TV Config Updates

--- apps/models/config.py
+++ apps/models/config.py
@@ -6,7 +6,13 @@ class TvConfig(models.Model):
     api_key_trakt = models.CharField('Trakt API Key', max_length=254, blank=True)
     api_key_tvdb = models.CharField('TVDB API Key', max_length=254, blank=True)
 
-    suggestions_enabled = models.BooleanField('Suggestions Enabled', default=True)
-    suggestions_trending_enabled = models.BooleanField('Trending Shows Enabled', default=True)
-    suggestions_popular_enabled = models.BooleanField('Popular Shows Enabled', default=True)
-    suggestions_anticipated_enabled = models.BooleanField('Anticipated Shows Enabled', default=True)
+    suggestions_trending_enabled = models.BooleanField('Trending Shows', default=True)
+    suggestions_popular_enabled = models.BooleanField('Popular Shows', default=True)
+    suggestions_anticipated_enabled = models.BooleanField('Anticipated Shows', default=True)
+
+    def suggestions_enabled(self):
+        "Returns true if any suggestions are enabled"
+        if suggestions_trending_enabled or suggestions_popular_enabled or suggestions_anticipated_enabled:
+            return True
+        else:
+            return False
