7c69b7c7aff0ef3be1595e028f55ee918c0efb67
oliversscripts <olivers.private@gmail.com>
2018-09-27 06:10:50 -0400

    Added django-dynamic-preferences

--- apps/models/config.py
+++ apps/models/config.py
@@ -1,2 +1,6 @@
 from django.db import models
+import dbsettings
 
+class TvOptions(dbsettings.Group):
+    enabled = dbsettings.BooleanValue('is tv functionality enabled', default=True)
+    trakt_api_key = dbsettings.StringValue()
