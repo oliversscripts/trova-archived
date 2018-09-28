20c70a46b8b5e0433f69ef0a43a191684933b38b
oliversscripts <olivers.private@gmail.com>
2018-09-28 13:01:47 -0400

    Modified tv_config model

--- apps/models/config.py
+++ apps/models/config.py
@@ -3,7 +3,7 @@ from django.db import models
 class TvConfig(models.Model):
 
     # General
-    enabled = models.BooleanField('Enabled', default=True)
+    tv_enabled = models.BooleanField('Enabled', default=True)
     
     # APIs
     api_key_trakt = models.CharField('Trakt API Key', max_length=254, blank=True)
@@ -16,18 +16,18 @@ class TvConfig(models.Model):
 
     # Sonarr
     sonnar_host = models.CharField('Sonarr IP or Hostname', max_length=254, blank=True, default='localhost')
-    sonnar_port = models.IntegerField('Sonarr Port', blank=True, default=8989)
+    sonnar_port = models.IntegerField('Sonarr Port', blank=True, null=True, default='8989')
     sonnar_api_key = models.CharField('Sonarr API Key', max_length=254, blank=True)
-    sonarr_sub_directory = models.CharField('Sonarr Sub-directory', max_length=254, blank=True, default='localhost')
+    sonarr_sub_directory = models.CharField('Sonarr URL Path', max_length=254, blank=True)
     sonarr_ssl = models.BooleanField('Sonarr SSL Enabled', default=False)
-    sonarr_quality_profile = models.IntegerField('Sonarr Quality Profile', blank=True, default=1)
-    sonarr_tv_dir = models.CharField('Sonarr TV Directory', max_length=254, blank=True)
+    sonarr_quality_profile = models.IntegerField('Sonarr Quality Profile', blank=True, null=True)
+    sonarr_tv_dir = models.CharField('Sonarr TV Folder Root', max_length=254, blank=True)
     sonarr_season_folders = models.BooleanField('Sonarr Season Folders', default=False)
 
     # Model definitions
-    def suggestions_enabled(self):
+    def tv_suggestions_enabled(self):
         "Returns true if any suggestions are enabled"
-        if suggestions_trending_enabled or suggestions_popular_enabled or suggestions_anticipated_enabled:
+        if self.suggestions_trending_enabled or self.suggestions_popular_enabled or self.suggestions_anticipated_enabled:
             return True
         else:
             return False
