41f0863f135ff4e8b34dd43d73070791c2acad34
oliversscripts <olivers.private@gmail.com>
2018-10-01 12:25:51 -0400

    Added Request ID to request models

--- apps/models/request.py
+++ apps/models/request.py
@@ -1,17 +1,17 @@
 from apps.libs import *
 
 class TvRequest(models.Model):
-    request_id = models.AutoField(primary_key=True)
+    request_id = models.AutoField('Request Id', primary_key=True)
     requested_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
 class MovieRequest(models.Model):
-    request_id = models.AutoField(primary_key=True)
+    request_id = models.AutoField('Request Id', primary_key=True)
     requested_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class MusicRequest(models.Model):
-    request_id = models.AutoField(primary_key=True)
+    request_id = models.AutoField('Request Id', primary_key=True)
     requested_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class BookRequest(models.Model):
-    request_id = models.AutoField(primary_key=True)
+    request_id = models.AutoField('Request Id', primary_key=True)
     requested_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
