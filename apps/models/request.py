d22ad1e969f585dac965036a07c37c2eddd55580
oliversscripts <olivers.private@gmail.com>
2018-10-05 13:54:44 -0400

    TvRequest model cleanup

--- apps/models/request.py
+++ apps/models/request.py
@@ -3,8 +3,8 @@ from apps.libs import *
 class TvRequest(models.Model):
     request_id = models.AutoField('Request Id', primary_key=True)
     sonarr_id = models.IntegerField(unique=True, db_index=True, blank=False)
-    title = models.CharField('Show Name', max_length=254, blank=True)
     requested_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField('Show Name', max_length=254, blank=True)
     
 class MovieRequest(models.Model):
     request_id = models.AutoField('Request Id', primary_key=True)
