80ee83d12896469f88237cd5b291b7bb1f3b7015
oliversscripts <olivers.private@gmail.com>
2018-10-08 13:52:17 -0400

    Added manager and admin flags to user model

--- apps/models/user.py
+++ apps/models/user.py
@@ -2,7 +2,14 @@ from apps.libs import *
 
 class User(AbstractUser):
     plex_id = models.CharField(max_length=254, blank=True)
-    trova_admin = models.BooleanField(default=False)
+    manager = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s' % (self.username)
+    
+    def is_admin(self):
+        return self.admin
+    
+    def is_manager(self):
+        return self.manager
